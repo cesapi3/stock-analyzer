9f7cb971fae08265d2b61fbf7a8866ac916881e3
Cesare Piras <cesarepiras03@gmail.com>
2026-03-04 19:00:42 +0100

    Create main React component for App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Stock Analyzer</h1>
+      <p>Analyze your stock investments effectively.</p>
+    </div>
+  );
+};
+
+export default App;
